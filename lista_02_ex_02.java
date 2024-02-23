93dca214251f9ff7c8fb87ccc20b2f46a3d492ac
ryanp2dev <65263907+ryanp2dev@users.noreply.github.com>
2024-02-22 21:46:08 -0400

    Create lista_02_ex_02.java

---
+++ lista_02_ex_02.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] qnumeros = new int[20];
+        int qn = 1;
+        int qPositivos = 0;
+
+//    int[] qTotalP = new int[qPositivosi];
+        for (int i = 0; i <=19 ;i++){
+            System.out.println("Digite o "+ (qn + i)+ ":");
+            qnumeros[i] = in.nextInt();
+            if(qnumeros[i] > 0) {
+                qPositivos +=1;
+            }
+
+        }
+
+
+        System.out.println("numeros posivos: " + qPositivos);
+
+        for(int i = 0; i<=19;i++){
+            if(qnumeros[i] >0) {
+                System.out.println("OS numeros Posivos sao " + qnumeros[i]);
+            }
+        }
+    }
+}
